0dbf8f01524ab4a19d75e1279b6cb1e86d67ae3c
Ajay Gandhi <aag255@cornell.edu>
2017-12-01 19:21:13 -0800

    Try to use actions on google

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 'use strict';
 
-const express     = require('express'),
-      bodyParser  = require('body-parser'),
-      rp          = require('request-promise'),
-      parseString = require('xml2js').parseString;
+const express       = require('express'),
+      bodyParser    = require('body-parser'),
+      rp            = require('request-promise'),
+      DialogflowApp = require('actions-on-google').DialogflowApp;
+      parseString   = require('xml2js').parseString;
 
 // Set up express
 const app = express();
 app.use(bodyParser.json());
 app.set('port', (process.env.PORT || 8000));
 
+const WELCOME_INTENT = 'input.welcome';
 const base = 'http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=sf-muni&stopId=';
+
 app.post('/kt', function (req, res) {
+  const app = new DialogflowApp({ request: req, response: res });
+  app.handleRequest(welcomeIntent);
+
+  /*
   rp(base + req.body.result.parameters.direction)
   .then((response) => {
     parseString(response, (err, result) => {
@@ -28,7 +35,27 @@ app.post('/kt', function (req, res) {
   .catch(() => {
     res.send("error");
   });
+  */
 });
+
+function welcomeIntent(app) {
+  rp(base + app.getArgument(WELCOME_INTENT))
+    .then((response) => {
+      parseString(response, (err, result) => {
+        if (err) {
+          console.error(err);
+          app.tell('Error getting information');
+        }
+
+        const predictions = result.body.predictions[0].direction.pop().prediction;
+        const firstPrediction = predictions[0].$.minutes;
+        app.tell('The next train will be in ' + firstPrediction + ' minutes.');
+      });
+    })
+    .catch(() => {
+      app.tell('Error getting information');
+    });
+}
 
 app.listen(app.get('port'), function () {
   console.log('Serving on port', app.get('port'));
